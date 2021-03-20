3e300ae44c0d1081b798b827c4ac1fa266bcc8e4
Utku Guclu <stanger307@live.com>
2021-03-20 08:20:47 -0700

    fizzBUzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,9 +3,9 @@ let answer = parseInt(prompt("Enter a number, fizzy dizzy: "));
 for (let i = 1; i <= answer; i++){
     if (i % 3 === 0 && i % 5 === 0){
         console.log("FizzDizz");
-    } else if (answer % 3 === 0) {
+    } else if (i % 3 === 0) {
         console.log("Fizz");
-    } else if (answer % 5 === 0) { 
+    } else if (i % 5 === 0) { 
         console.log("Dizz");
     } else {
         console.log(i);
